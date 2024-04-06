9b388d6998c52511b3e94262b62d243af9fbd8c3
Aditya Kumar <kohsa9211420@gmail.com>
2024-04-06 06:22:44 -0700

    starter app:

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+//headers
+
+//main app
+const app = express();
+app.use(cors());
+dotenv.config({ path: "./config.env" });
+
+//middlewares
+
+//db connection
+require("./DB/conn");
+
+//api
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("welcome to the backend ");
+});
+app.listen(PORT, (req, res) => {
+  console.log(`listening on PORT ${PORT}`);
+});
